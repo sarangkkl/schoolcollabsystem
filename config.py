979df90350cfa0bb22df749499480a9ddc22a7d6
Tejas Jagannatha <tejasbiker23@gmail.com>
2023-07-03 22:12:23 +0100

    ConfirmationMail-HTML

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 # This is the Config file
-
 from pymongo import MongoClient
 
-
-
 URI_CONNECTION = "mongodb+srv://tejas22t:Coder123%21@mycluster.gmaql.mongodb.net/?ssl=true&ssl_cert_reqs=CERT_NONE"
-
 Client = MongoClient(URI_CONNECTION)
--- schoolapp.py
+++ schoolapp.py
@@ -3,12 +3,14 @@ from blueprints.add_studentblueprint import student_blueprint
 from blueprints.landingpageblueprint import landingPage_blueprint
 from flask_mail import Mail
 from config import Client
+
 #--- Flask Setup --#
 app = Flask(__name__)
 app.register_blueprint(student_blueprint)
 app.register_blueprint(landingPage_blueprint)
 
 #--- Config controls ---#
+app.static_folder = 'static'
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'tejasjagannatha@gmail.com'
