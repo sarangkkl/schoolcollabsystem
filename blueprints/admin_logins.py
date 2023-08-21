3c44333701c15d5c631a29165b77a1e3520776a9
Tejas Jagannatha <tejasbiker23@gmail.com>
2023-08-21 22:53:43 +0100

    'building-admin-make-announcements'

--- blueprints/admin_logins.py
+++ blueprints/admin_logins.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, request, render_template
+from config import Client
+import datetime
 
 UL_ADMIN_CODE = '123456'
 DCU_ADMIN_CODE= '654321'
@@ -24,9 +26,40 @@ def ul_login_admin():
 
 
 
-@admin_blueprint.route('/ul-makeannouncement', methods=['POST'])
+@admin_blueprint.route('/make_announcement', methods=['POST'])
 def ul_make_announcement():
     if request.method == "POST":
-        print("ULLLLLLL")
         return render_template("university/make_announcement.html")
-    pass
+
+@admin_blueprint.route('/submit-announcement', methods= ['POST'])
+def submit_announcement():
+    if request.method == "POST":
+        current_timestamp = datetime.datetime.now()
+        current_date = current_timestamp.date()
+        current_time = current_timestamp.time()
+        formatted_date = current_date.strftime('%Y-%m-%d')
+        formatted_time = current_time.strftime('%H:%M:%S')
+
+        title= request.form['title']
+        description= request.form['description']
+        db = Client["studentdata"]
+
+        # Choose the collection
+        collection = db["Admin_announcement"]
+
+        announcement_json= {'title': title, 'description': description, 'timestamp': formatted_date + ' ;' + formatted_time}
+        collection.insert_one(announcement_json)
+        announcements = collection.find()
+        print(announcements)
+        return '<h4> Thanks the announcement has been made '
+
+@admin_blueprint.route('/view_announcement', methods= ['GET'])
+def view_announcement():
+
+
+    db = Client["studentdata"]
+    collection = db["Admin_announcement"]
+    announcements = list( collection.find())
+
+    return render_template('university/view_announcement.html',data= announcements)
+
--- blueprints/landingpageblueprint.py
+++ blueprints/landingpageblueprint.py
@@ -4,73 +4,70 @@ import pika
 from config import Client
 from passlib.hash import sha256_crypt
 from flask_mail import Mail, Message
-import pyotp, time, json, threading
+import pyotp, time, threading
 from MFA import generate_otp
 
 def producer(message):
-    connection_parameters= pika.ConnectionParameters('localhost')
-    connection= pika.BlockingConnection(connection_parameters)
-    chanel= connection.channel()
-    chanel.queue_declare(queue= 'login')
-
-    chanel.basic_publish(exchange= '', routing_key= 'login', body= message)
-    print("Message:", message)
-    connection.close()
-
-def on_message_recieved(ch, method, properties, body):
-    print("Message:", body)
-
-
-def consumer():
     connection_parameters = pika.ConnectionParameters('localhost')
     connection = pika.BlockingConnection(connection_parameters)
-    chanel = connection.channel()
-    chanel.queue_declare(queue='login')
-    chanel.basic_consume(queue='login', auto_ack=True, on_message_callback=on_message_recieved)
-
-    chanel.start_consuming()  # Add this line to start consuming messages
-
+    channel = connection.channel()
+    channel.queue_declare(queue='login')
+    channel.basic_publish(exchange='', routing_key='login', body=message)
     connection.close()
 
+
+def consumer(connection_parameters):
+    connection = pika.BlockingConnection(connection_parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue='login')
+
+    def on_message_received(ch, method, properties, body):
+        print("Message:", body)
+        # Process the message here
+        ch.basic_ack(delivery_tag=method.delivery_tag)  # Acknowledge the message
+
+    channel.basic_consume(queue='login', on_message_callback=on_message_received)
+    channel.start_consuming()
+
+
+def consume_single_message(channel):
+    queue_name = 'login'
+    method_frame, header_frame, body = channel.basic_get(queue=queue_name)
+
+    if method_frame:
+        print("Received message:", body)
+        # Process the message here
+
+        # Acknowledge the message
+        channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+    else:
+        print("No message in the queue.")
 
 
 landingPage_blueprint = Blueprint('landingpage', __name__)
+
+
 @landingPage_blueprint.route('/SCS', methods=['GET', 'POST'])
 def landingpage_route():
     if request.method == "POST":
         name = request.form['username']
         password = request.form['password']
-
         db_newstudent = Client["studentdata"]
         registered_student = db_newstudent["registeredstudents"]
 
-        #Gets student details using name
         def get_student_by_name(name):
-            try:
-                filter = {
-                    'name': name,
-                }
-                # Execute the query
-                result = registered_student.find_one(filter, {'_id': 0})
-                result= {'result': result}
-                return result['result']
+            filter = {'name': name}
+            result = registered_student.find_one(filter, {'_id': 0})
+            return result
 
-            except:
-                #Handle HTTP No data found
-                return None
+        resultname = get_student_by_name(name)
 
-        resultname= get_student_by_name(name)
-
-        print("this is result", resultname)
         if resultname:
             stored_password = resultname['password']
-            stored_emailid= resultname['emailid']
-
-            # Hash the entered password using the same algorithm and encoding as the stored password
+            stored_emailid = resultname['emailid']
             sha256_hash = hashlib.sha256()
             sha256_hash.update(password.encode('utf-8'))
             hashed_password = sha256_hash.hexdigest()
-            valid = 0
 
             if hashed_password == stored_password:
                 from schoolapp import app
@@ -80,65 +77,38 @@ def landingpage_route():
                 msg.html = render_template('mailingsystem/sendotp.html', code=key_val)
                 mail.send(msg)
                 session['token'] = key_obj
-                print("Key obj: ",key_obj)
-                valid= 1
-                print('valid')
+                valid = 1
                 producer(stored_emailid)
-                return render_template('mfa_check.html', emailid= stored_emailid, isvalid= valid)
-
+                return render_template('mfa_check.html', emailid=stored_emailid, isvalid=valid)
             else:
                 valid = 0
-                print('Invalid')
                 producer(stored_emailid)
-
                 return render_template('mfa_check.html', emailid=None, isvalid=valid)
-
         else:
-            valid= 0
-
-            return render_template('mfa_check.html', emailid= None, isvalid=valid)
-
+            valid = 0
+            return render_template('mfa_check.html', emailid=None, isvalid=valid)
     else:
-
         return render_template('scs.html')
 
 
 @landingPage_blueprint.route('/resetuser', methods=['POST', 'GET'])
-# Reset password
 def reset_user():
-    print('as')
     if request.method == "POST":
-        email= request.form['regid']
-
+        email = request.form['regid']
         db_newstudent = Client["studentdata"]
         registered_student = db_newstudent["registeredstudents"]
 
+        def get_student_by_email(email):
+            filter = {'emailid': email}
+            result = registered_student.find_one(filter, {'_id': 0})
+            return result
 
-        # Gets student details using name
-        def get_student_byemail(email):
-            try:
-                filter = {
-                    'emailid': email,
-                }
-                # Execute the query
-                result = registered_student.find_one(filter, {'_id': 0})
-                print(result)
-                return result
-            except:
-                # Handle HTTP No data found
-                return None
-        result= get_student_byemail(email)
+        result = get_student_by_email(email)
+
         if result:
-            #your username is this
             session['password'] = result['password']
             session['name'] = result['name']
-
-            print( result['password'])
-
-            #Change password?
-            return render_template('passwordreset.html', password= result['password'], name= result['name'])
-
-        #if email data is not found
+            return render_template('passwordreset.html', password=result['password'], name=result['name'])
         else:
             return render_template('email_notfound.html')
     else:
@@ -146,25 +116,33 @@ def reset_user():
 
 
 @landingPage_blueprint.route('/changepassword', methods=['POST', 'GET'])
-# Reset password
 def change_password():
     if request.method == "POST":
-        print('post')
-        # Change request for password
-        existing_pass= session.get('password')
-        username= session.get('name')
-
-        new_pass= request.form['password']
-        print("new pass", new_pass)
+        existing_pass = session.get('password')
+        username = session.get('name')
+        new_pass = request.form['password']
         db_newstudent = Client["studentdata"]
         registered_student = db_newstudent["registeredstudents"]
-        # Hash the entered password using the same algorithm and encoding as the stored password
         sha256_hash = hashlib.sha256()
         sha256_hash.update(new_pass.encode('utf-8'))
         hashed_password = sha256_hash.hexdigest()
         myquery = {"name": username}
-        newvalues = {"$set": {"password": hashed_password}  }
+        newvalues = {"$set": {"password": hashed_password}}
         registered_student.update_one(myquery, newvalues)
-        print("Updated")
-        #Successful Password changed
         return render_template('password_change_successful.html')
+
+
+def start_consumers():
+    connection_parameters = pika.ConnectionParameters('localhost')
+    connection = pika.BlockingConnection(connection_parameters)
+    channel = connection.channel()
+
+    # Declare the 'login' queue
+    channel.queue_declare(queue='login')
+
+    consumer_thread = threading.Thread(target=consumer, args=(connection_parameters,))
+    consumer_thread.start()
+    consume_single_message(channel)  # Consume a single message
+
+start_consumers()
+
