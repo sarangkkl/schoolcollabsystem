9e00cd9d96ad4d0aecad5a5824632e518b995c71
Gaurav Sah <sarangkkl2@gmail.com>
2023-09-07 16:06:31 +0530

    done

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # This is the Config file
 from pymongo import MongoClient
-
+import os
 URI_CONNECTION = "mongodb+srv://tejas22t:Coder123%21@mycluster.gmaql.mongodb.net/?ssl=true&ssl_cert_reqs=CERT_NONE"
 Client = MongoClient(URI_CONNECTION)
+media_path = os.path.join(os.getcwd(), 'media')
+files = os.listdir(media_path)
 
-
-PATH= r'/home/sarang/Desktop/projects/schoolcollabsystem/media'
+PATH= files
