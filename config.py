3bf2dfe3d7a378230eba1669288423d50339a681
Gaurav Sah <sarangkkl2@gmail.com>
2023-09-07 16:09:42 +0530

    done

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ import os
 URI_CONNECTION = "mongodb+srv://tejas22t:Coder123%21@mycluster.gmaql.mongodb.net/?ssl=true&ssl_cert_reqs=CERT_NONE"
 Client = MongoClient(URI_CONNECTION)
 media_path = os.path.join(os.getcwd(), 'media')
-files = os.listdir(media_path)
 
-PATH= files
+
+# print(media_path)
+
+PATH= media_path
